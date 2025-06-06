0129828b4c0ce87817353380c964c043005c1475
felGutierrez <fi.gutierrez@duocuc.cl>
2025-06-05 23:42:30 -0400

    buscar persona con model

--- src/app/pages/buscar-persona/buscar-persona.page.ts
+++ src/app/pages/buscar-persona/buscar-persona.page.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-
-interface Usuario {
-  id: string;
-  username: string;
-  userAvatar: string;
-  estado: boolean; // Indica si el usuario está en línea o no
-}
-
-
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-buscar-persona',
@@ -23,59 +15,211 @@ export class BuscarPersonaPage implements OnInit {
 
   // Lista original de chats, tipificada con la interface ChatPrivado
   todosusuarios: Usuario[] = [
-    { id: 'u133', username: 'techguru', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u134', username: 'catlover', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u135', username: 'bookworm', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u136', username: 'nightowl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u137', username: 'traveler', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u138', username: 'foodie', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u139', username: 'photoguy', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u140', username: 'runner', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u141', username: 'gamerchick', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u142', username: 'coder123', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u143', username: 'musician', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u144', username: 'hiker', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u145', username: 'artist', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u146', username: 'dancer', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u147', username: 'chefmaster', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u148', username: 'bookreader', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u149', username: 'fitnessfan', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u150', username: 'gadgetlover', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u151', username: 'swimmer', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u152', username: 'yogagirl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u153', username: 'skater', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u154', username: 'traveller', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u155', username: 'moviebuff', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u156', username: 'catmom', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u157', username: 'dogdad', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u158', username: 'pianoplayer', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u159', username: 'gardener', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u160', username: 'techlover', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u161', username: 'biker', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u162', username: 'painter', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u163', username: 'traveller', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u164', username: 'foodlover', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u165', username: 'runnergirl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u166', username: 'drummer', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u167', username: 'coderboy', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u168', username: 'skatergirl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u169', username: 'filmfan', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u170', username: 'writer', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u171', username: 'photogirl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u172', username: 'travelerboy', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u173', username: 'musicfan', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u174', username: 'yogaman', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u175', username: 'gamerboy', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u176', username: 'bookgirl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-    { id: 'u177', username: 'chef', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: true },
-    { id: 'u178', username: 'artistgirl', userAvatar: 'https://ionicframework.com/docs/img/demos/avatar.svg', estado: false },
-
-  ];
+  {
+    id_usuario: 133,
+    nombre_usuario: 'techguru',
+    correo_electronico: 'techguru@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 134,
+    nombre_usuario: 'catlover',
+    correo_electronico: 'catlover@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 135,
+    nombre_usuario: 'bookworm',
+    correo_electronico: 'bookworm@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 136,
+    nombre_usuario: 'nightowl',
+    correo_electronico: 'nightowl@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 137,
+    nombre_usuario: 'traveler',
+    correo_electronico: 'traveler@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 138,
+    nombre_usuario: 'foodie',
+    correo_electronico: 'foodie@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 139,
+    nombre_usuario: 'photoguy',
+    correo_electronico: 'photoguy@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 140,
+    nombre_usuario: 'runner',
+    correo_electronico: 'runner@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 141,
+    nombre_usuario: 'gamerchick',
+    correo_electronico: 'gamerchick@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 142,
+    nombre_usuario: 'coder123',
+    correo_electronico: 'coder123@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 143,
+    nombre_usuario: 'musician',
+    correo_electronico: 'musician@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 144,
+    nombre_usuario: 'hiker',
+    correo_electronico: 'hiker@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 145,
+    nombre_usuario: 'artist',
+    correo_electronico: 'artist@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 146,
+    nombre_usuario: 'dancer',
+    correo_electronico: 'dancer@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 147,
+    nombre_usuario: 'chefmaster',
+    correo_electronico: 'chefmaster@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 148,
+    nombre_usuario: 'bookreader',
+    correo_electronico: 'bookreader@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 149,
+    nombre_usuario: 'fitnessfan',
+    correo_electronico: 'fitnessfan@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 150,
+    nombre_usuario: 'gadgetlover',
+    correo_electronico: 'gadgetlover@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  },
+  {
+    id_usuario: 151,
+    nombre_usuario: 'swimmer',
+    correo_electronico: 'swimmer@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: false,
+    estado_online: true
+  },
+  {
+    id_usuario: 152,
+    nombre_usuario: 'yogagirl',
+    correo_electronico: 'yogagirl@correo.com',
+    fecha_registro: new Date('2024-01-01'),
+    contrasena: '',
+    avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+    estado_cuenta: true,
+    estado_online: true
+  }
+]
+;
 
   usuarios: Usuario[] = [];
   busquedaActiva: boolean = false;
-
-
 
   constructor() { }
 
@@ -96,7 +240,7 @@ export class BuscarPersonaPage implements OnInit {
     }
 
     this.usuarios = this.todosusuarios.filter(usuario =>
-      usuario.username.toLowerCase().includes(query)
+      usuario.nombre_usuario.toLowerCase().includes(query)
     );
   }
 
