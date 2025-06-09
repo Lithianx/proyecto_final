a251383fa9d75f4fa97ff3d1fb4e4f80c338f4dc
Lithianx <cr.opazoc@duocuc.cl>
2025-06-09 00:24:25 -0400

    token de firebase en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig : {
+  apiKey: "AIzaSyCZT-KIRYdhH0zHtL0_3ZYmPDW9kUgFNwI",
+  authDomain: "app-eventos-7f5a8.firebaseapp.com",
+  projectId: "app-eventos-7f5a8",
+  storageBucket: "app-eventos-7f5a8.firebasestorage.app",
+  messagingSenderId: "940926776806",
+  appId: "1:940926776806:web:6c39d6ec6874607d531227"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   giphyApiKey: 'JY1WOhcF9c6Vj8FSsht1WmSXgOhp9fpn',
+  
+  firebaseConfig : {
+  apiKey: "AIzaSyCZT-KIRYdhH0zHtL0_3ZYmPDW9kUgFNwI",
+  authDomain: "app-eventos-7f5a8.firebaseapp.com",
+  projectId: "app-eventos-7f5a8",
+  storageBucket: "app-eventos-7f5a8.firebasestorage.app",
+  messagingSenderId: "940926776806",
+  appId: "1:940926776806:web:6c39d6ec6874607d531227"
+}
 };
 
 /*
